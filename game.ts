5cd1afbcecc4ded5e0d6b50b9846c3ec4da25453
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-17 01:13:13 +0500

    NUMBER_GUESSING_GAME

---
+++ game.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+
+type ansType = {
+    userGuess: number
+}
+
+const systemGeneratedNo = Math.floor(
+    Math.random()* 100);
+
+const answer: ansType = await inquirer.prompt([
+    {
+      type: "number",
+      name: "userGuess",
+      message: "write your guess:"
+}])
+
+const {userGuess} = answer;
+if (userGuess === systemGeneratedNo){
+    console.log(userGuess,"userGuess", systemGeneratedNo , 'SYs')
+    console.log("Yeahh Your Answer Is Correct \n YOU WIN !")
+} else{
+    console.log("Please try again GOOD LUCK next time!")
+}
